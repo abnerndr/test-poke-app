143f82a4ad517cf0727136a2b3f5a047331db198
Abner A Ananias <abner.ananias@uds.com.br>
2025-12-17 11:27:03 -0300

    Implement loading states for Pokémon pages using Suspense and LoadingScreen component. Wrap PokémonList and PokémonDetails in Suspense to enhance user experience during data fetching.

--- src/app/pokemon/[id]/page.tsx
+++ src/app/pokemon/[id]/page.tsx
@@ -1,5 +1,11 @@
 import PokemonDetails from "@/components/app/pokemon/details";
+import { Suspense } from "react";
+import LoadingScreen from "@/components/common/loading-screen";
 
 export default function PokemonDetailPage() {
-	return <PokemonDetails />;
+	return (
+		<Suspense fallback={<LoadingScreen />}>
+			<PokemonDetails />
+		</Suspense>
+	);
 }
--- src/app/pokemon/page.tsx
+++ src/app/pokemon/page.tsx
@@ -1,5 +1,11 @@
 import PokemonList from "@/components/app/pokemon/list";
+import { Suspense } from "react";
+import LoadingScreen from "@/components/common/loading-screen";
 
 export default function PokemonPage() {
-	return <PokemonList />;
+	return (
+		<Suspense fallback={<LoadingScreen />}>
+			<PokemonList />
+		</Suspense>
+	);
 }
